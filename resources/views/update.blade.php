82b6998337507c9b2d9b2110a86b47ebfd6c07ff
KubaGithub92 <jakub.rosol@email.cz>
2022-10-21 16:06:45 +0200

    added links to owners

--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -7,6 +7,7 @@
   <title>UPDATE INFO</title>
 </head>
 <body>
+  @if($animal->id)
     <form action="{{route("animal.update",$animal->id)}}"  method="post">
       @method('PUT')
       @csrf
@@ -20,8 +21,27 @@
       <input type="text" name="age" value="{{$animal->age}}">
       <div>WEIGHT:</div>
       <input type="text" name="weight" value="{{$animal->weight}}">
+   
+  @else 
+      <form action="{{route("animal.update",$animal->id)}}"  method="post">
 
-      <button type="submit">Confirm update</button>
+      @csrf
+      <div>NAME:</div>
+      <input type="text" name="name" value="">
+      <div>SPECIES:</div>
+      <input type="text" name="species" value="">
+      <div>BREED:</div>
+      <input type="text" name="breed" value="">
+      <div>AGE:</div>
+      <input type="text" name="age" value="">
+      <div>WEIGHT:</div>
+      <input type="text" name="weight" value="">
+  
+  @endif
+  
+  
+
+      <button type="submit">{{$animal->id ? 'Confirm update' : 'Add animal'}}</button>
       
     </form>
 </body>
